2c8f30c9a1285152ed48bfa72c04fa5f362dbb76
Miloš Hadžić <milos@rightfold.io>
2016-08-24 12:39:17 +0200

    Merge sort and insertion sort

---
+++ src/lib.rs
@@ -0,0 +1,126 @@
+pub fn merge_sort<T: Ord + Copy>(arr: &mut [T]) {
+    match arr.len() {
+        0 => return,
+        1 => return,
+        n => {
+            let mid = n / 2;
+            {
+                let (left, right) = arr.split_at_mut(mid);
+
+                merge_sort(left);
+                merge_sort(right);
+            }
+            merge(arr, mid)
+        }
+    }
+}
+
+fn merge<T: Ord + Copy>(arr: &mut [T], splitpoint: usize) {
+    // TODO: Try to do the in-place version
+    let mut buf = Vec::new();
+    let (mut i, mut j) = (0, splitpoint);
+
+    while buf.len() < arr.len() {
+        if i == splitpoint {
+            buf.extend_from_slice(&arr[j..]);
+            break;
+        }
+
+        if j == arr.len() {
+            buf.extend_from_slice(&arr[i..splitpoint]);
+            break;
+        }
+
+        if arr[i] <= arr[j] {
+            buf.push(arr[i]);
+            i += 1;
+        } else {
+            buf.push(arr[j]);
+            j += 1;
+        }
+
+    }
+
+    for (el, bufel) in arr.iter_mut().zip(buf.iter()) {
+        *el = *bufel;
+    }
+}
+
+pub fn insertion_sort<T: Ord + Copy>(arr: &mut [T]) {
+    for i in 1..arr.len() {
+        let mut j = i;
+        while j > 0 && arr[j - 1] > arr[j] {
+            let t = arr[j].clone();
+            arr[j] = arr[j - 1];
+            arr[j - 1] = t;
+            j -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate rand;
+    use super::*;
+    use self::rand::Rng;
+    use std::cmp::Ordering;
+
+    // For testing stability
+    #[derive(Eq, Debug, Copy, Clone)]
+    struct Pair(i32, i32);
+    impl Ord for Pair {
+        fn cmp(&self, other: &Pair) -> Ordering {
+            self.0.cmp(&other.0)
+        }
+    }
+
+    impl PartialOrd for Pair {
+        fn partial_cmp(&self, other: &Pair) -> Option<Ordering> {
+            Some(self.0.cmp(&other.0))
+        }
+    }
+
+    impl PartialEq for Pair {
+        fn eq(&self, other: &Pair) -> bool {
+            self.0 == other.0 && self.1 == other.1
+        }
+    }
+
+    #[test]
+    fn test_mergesort() {
+        let mut rng = rand::thread_rng();
+        let mut vec: Vec<i32> = (0..50).map(|_| rng.gen::<i32>() % 100).collect();
+        let mut sorted = vec.clone();
+        sorted.sort();
+        merge_sort(vec.as_mut_slice());
+        assert_eq!(sorted, vec);
+    }
+
+    #[test]
+    fn test_insertion_sort() {
+        let mut rng = rand::thread_rng();
+        let mut vec: Vec<i32> = (0..50).map(|_| rng.gen::<i32>() % 100).collect();
+        let mut sorted = vec.clone();
+        sorted.sort();
+        insertion_sort(vec.as_mut_slice());
+        assert_eq!(sorted, vec);
+    }
+
+    #[test]
+    fn test_merge_sort_stability() {
+        let mut vec: Vec<Pair> = vec![Pair(6, 3), Pair(5, 5), Pair(6, 1), Pair(1, 3)];
+        let mut c = vec.clone();
+        c.sort();
+        merge_sort(vec.as_mut_slice());
+        assert_eq!(c, vec);
+    }
+
+    #[test]
+    fn test_insertion_sort_stability() {
+        let mut vec: Vec<Pair> = vec![Pair(6, 3), Pair(5, 5), Pair(6, 1), Pair(1, 3)];
+        let mut c = vec.clone();
+        c.sort();
+        insertion_sort(vec.as_mut_slice());
+        assert_eq!(c, vec);
+    }
+}
